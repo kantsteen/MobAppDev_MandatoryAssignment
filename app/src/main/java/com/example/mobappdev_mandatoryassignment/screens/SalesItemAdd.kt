1537d4ceb26c7133995d3d86c4f2b79d9eecbd7b
Lasse Egebo <lae003@edu.zealand.dk>
2025-10-10 11:39:14 +0200

    Added SalesItemDetails screen basics

---
+++ app/src/main/java/com/example/mobappdev_mandatoryassignment/screens/SalesItemAdd.kt
@@ -0,0 +1,4 @@
+package com.example.mobappdev_mandatoryassignment.screens
+
+class SalesItemAdd {
+}
--- app/src/main/java/com/example/mobappdev_mandatoryassignment/screens/SalesItemDetails.kt
+++ app/src/main/java/com/example/mobappdev_mandatoryassignment/screens/SalesItemDetails.kt
@@ -1,11 +1,21 @@
 package com.example.mobappdev_mandatoryassignment.screens
 
 import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -14,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobappdev_mandatoryassignment.model.SalesItem
 import java.time.LocalDateTime
@@ -29,20 +40,56 @@ fun SalesItemDetails(
     var description by remember { mutableStateOf(salesItem.description) }
     var priceStr by remember { mutableStateOf(salesItem.price.toString()) }
     Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
+        modifier = modifier.fillMaxSize(), topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    ContentColor = MaterialTheme.colorScheme.primary
-                )
-            ),
-            description = { Text("SalesItem details!") }
-        }) {
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = { Text("SalesItem Details") })
 
+        }) { innerPadding ->
+        // TODO show error message
+        Column(modifier = modifier.padding(innerPadding)) {
+            // TODO layout for landscape
+            // TODO add and details are very similar
+            TextField(
+                onValueChange = { description = it },
+                value = description,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text("Descriptiom") })
+            TextField(
+                onValueChange = { priceStr = it },
+                value = priceStr,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text("Price") })
+            Row(
+                modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { onNavigateBack() }) {
+                    Text("Back")
+                }
+                Button(onClick = {
+                    // TODO validation
+                    val data = SalesItem(
+                        description = description,
+                        price = priceStr.toDouble(),
+                        id = TODO(),
+                        sellerMail = TODO(),
+                        sellerPhone = TODO(),
+                        time = TODO(),
+                        pictureUrl = TODO()
+                    )
+                    onUpdate(salesItem.id, data)
+                    onNavigateBack()
+                }) {
+                    Text("Update")
+                }
+            }
+        }
     }
-
-
 }
 
 
