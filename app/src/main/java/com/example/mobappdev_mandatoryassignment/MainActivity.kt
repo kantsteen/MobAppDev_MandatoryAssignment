5bf754dfa90a43141f519beb4feb0e7e3e841467
Lasse Egebo <lae003@edu.zealand.dk>
2025-10-10 13:53:38 +0200

    Working on top to bottom of app REST API to screen

--- app/src/main/java/com/example/mobappdev_mandatoryassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobappdev_mandatoryassignment/MainActivity.kt
@@ -24,6 +24,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.navigation.compose.rememberNavController
+import com.example.mobappdev_mandatoryassignment.model.SalesItem
+import com.example.mobappdev_mandatoryassignment.model.SalesItemsViewModel
 import com.example.mobappdev_mandatoryassignment.ui.theme.MobAppDev_MandatoryAssignmentTheme
 import kotlin.String
 import kotlin.collections.List
@@ -38,6 +42,7 @@ class MainActivity : ComponentActivity() {
 //                SalesItem("BMW", 100000)
 //                SalesItems()
 
+
             }
         }
     }
@@ -45,73 +50,84 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun SalesItem(description: String, price: Int, modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp)
-    ) {
-        Column(modifier) {
-
-            val image = painterResource(R.drawable.bmw_blue_sample)
-            Image(
-                painter = image,
-                contentDescription = null
-            )
-            Text(
-                "$description \n$price DKK",
-                modifier = modifier
-            )
-        }
-    }
-}
-
-@Composable
-fun SalesItems(
+fun MainScreen(
     modifier: Modifier = Modifier,
-    descriptions: List<String> = List(50) { "$it" }
-) {
-    LazyColumn(Modifier.padding(vertical = 4.dp)) {
-        items(items = descriptions) { description ->
-            Row(
-                modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.Top){
-                SalesItem(description = "BMW", 1000)
-            }
-        }
-    }
+    viewModel: SalesItemsViewModel = SalesItemsViewModel()
+    ) {
+val navController = rememberNavController()
+val salesItems = viewModel.salesItems.value
+val errorMessage = viewModel.errorMessage.value
 }
 
-@Preview
-@Composable
-fun SalesItemsPreview() {
-    MobAppDev_MandatoryAssignmentTheme {
-        SalesItems()
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun SalesItemPreview() {
-    Column(modifier = Modifier) {// can maybe be deleted
-        Row(modifier = Modifier) {
-            SalesItem("BMW", 100000)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobAppDev_MandatoryAssignmentTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun SalesItem(description: String, price: Int, modifier: Modifier = Modifier) {
+//    Surface(
+//        color = MaterialTheme.colorScheme.primary,
+//        modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp)
+//    ) {
+//        Column(modifier) {
+//
+//            val image = painterResource(R.drawable.bmw_blue_sample)
+//            Image(
+//                painter = image,
+//                contentDescription = null
+//            )
+//            Text(
+//                "$description \n$price DKK",
+//                modifier = modifier
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun SalesItems(
+//    modifier: Modifier = Modifier,
+//    descriptions: List<String> = List(50) { "$it" }
+//) {
+//    LazyColumn(Modifier.padding(vertical = 4.dp)) {
+//        items(items = descriptions) { description ->
+//            Row(
+//                modifier.fillMaxWidth(),
+//                horizontalArrangement = Arrangement.SpaceEvenly,
+//                verticalAlignment = Alignment.Top){
+//                SalesItem(description = "BMW", 1000)
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun SalesItemsPreview() {
+//    MobAppDev_MandatoryAssignmentTheme {
+//        SalesItems()
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun SalesItemPreview() {
+//    Column(modifier = Modifier) {// can maybe be deleted
+//        Row(modifier = Modifier) {
+//            SalesItem("BMW", 100000)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MobAppDev_MandatoryAssignmentTheme {
+//        Greeting("Android")
+//    }
+//}
--- app/src/main/java/com/example/mobappdev_mandatoryassignment/screens/SalesItemAdd.kt
+++ app/src/main/java/com/example/mobappdev_mandatoryassignment/screens/SalesItemAdd.kt
@@ -1,4 +1,29 @@
 package com.example.mobappdev_mandatoryassignment.screens
 
-class SalesItemAdd {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.mobappdev_mandatoryassignment.model.SalesItem
+import java.time.LocalDateTime
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SalesItemAdd(
+    modifier: Modifier = Modifier,
+    addSalesItem: (SalesItem) -> Unit = {},
+    onNavigateBack: () -> Unit = {}
+){
+    var description by remember { mutableStateOf("") }
+    var priceStr by remember { mutableStateOf("") }
+    var sellerEmail by remember { mutableStateOf("") }
+    var sellerPhone by remember { mutableStateOf("") }
+    var time by remember { mutableStateOf(LocalDateTime.now()) }
+    var pictureUrl by remember { mutableStateOf("") }
+
 }
